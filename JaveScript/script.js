a7d5436ab656559c776f1c3f0fd280f9c61712db
abeer6 <108021869+abeer6@users.noreply.github.com>
2022-07-03 12:28:36 +0300

    my cw4

---
+++ JaveScript/script.js
@@ -0,0 +1,23 @@
+alert("مرحبا بك في موقعي أنا عبير الفضلي،وسوف احسب درجتك");
+let grade = prompt("ادخل درجتك");
+console.log(grade);
+
+
+if(grade>=90)
+{console.log("لقد حصلت على امتياز");}
+else if (grade>=80)
+{console.log("لقد حصلت على جيد جدا");}
+else if(grade>=70)
+{console.log("لقد حصلت على جيد");}
+else if(grade>=60)
+{console.log("لقد حصلت على مقبول");}
+else if(grade>=50)
+{console.log("لقد حصلت على ضعيف");}
+ else
+{console.log("راسب");}
+
+
+
+//جافا سكربت ليست جافا
+
+
